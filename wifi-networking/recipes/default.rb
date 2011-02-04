4820dcf0333d57565a5ba52d8758ed256445e448
Phil Cohen <github@phlippers.net>
2011-02-04 13:14:03 -0800

    network-manager doesn't support 'service' resource at the moment

--- wifi-networking/recipes/default.rb
+++ wifi-networking/recipes/default.rb
@@ -6,8 +6,3 @@
 %w[network-manager network-config wpasupplicant wpagui].each do |pkg|
   package pkg
 end
-
-service "network-manager" do
-  supports :restart => true
-  action [ :enable, :start ]
-end
