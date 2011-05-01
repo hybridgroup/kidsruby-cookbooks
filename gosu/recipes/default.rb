d96ef80a57e4e3a97a5546effa5b4765e5a09657
Phil Cohen <github@phlippers.net>
2011-05-01 01:38:22 -0700

    add libfreeimage3 to gosu recipe

--- gosu/recipes/default.rb
+++ gosu/recipes/default.rb
@@ -3,6 +3,10 @@
 # Recipe:: default
 #
 
-%w[g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libaudiere-dev libsdl-ttf2.0-dev libsdl-mixer1.2 libsdl-mixer1.2-dev].each do |pkg|
+%w[
+    g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libaudiere-dev
+    libsdl-ttf2.0-dev libsdl-mixer1.2 libsdl-mixer1.2-dev
+    libfreeimage3 libfreeimage-dev
+  ].each do |pkg|
   package pkg
 end
