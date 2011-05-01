b6ca1cbe55445a0b90a45e7c13342e53bda358a4
Phil Cohen <github@phlippers.net>
2011-05-01 04:14:07 -0700

    remove libaudiere-dev from gosu for ubuntu natty

--- gosu/recipes/default.rb
+++ gosu/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 %w[
-    g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libaudiere-dev
+    g++ libgl1-mesa-dev libpango1.0-dev libboost-dev
     libsdl-ttf2.0-dev libsdl-mixer1.2 libsdl-mixer1.2-dev
     libfreeimage3 libfreeimage-dev
   ].each do |pkg|
