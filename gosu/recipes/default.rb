b521cc1032a8bffb6e7656446e6fcae09ac8349a
Phil Cohen <github@phlippers.net>
2011-04-30 23:24:00 -0700

    add libsdl-mixer packages to gosu recipe

--- gosu/recipes/default.rb
+++ gosu/recipes/default.rb
@@ -3,6 +3,6 @@
 # Recipe:: default
 #
 
-%w[g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libaudiere-dev libsdl-ttf2.0-dev].each do |pkg|
+%w[g++ libgl1-mesa-dev libpango1.0-dev libboost-dev libaudiere-dev libsdl-ttf2.0-dev libsdl-mixer1.2 libsdl-mixer1.2-dev].each do |pkg|
   package pkg
 end
